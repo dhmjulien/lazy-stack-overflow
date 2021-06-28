0ba13c45e8ec1f80bedca6a891111ee6d94bd9cc
dhmjulien <dhmjulien@gmail.com>
2021-06-28 16:55:32 +0200

    Fix scroll offset & code improvements 🔧

--- background.js
+++ background.js
@@ -1,5 +1,4 @@
 // Listen to a click event on the extension's button
 chrome.browserAction.onClicked.addListener(tab => {
-  // Send the message that the button has been clicked
   chrome.tabs.sendMessage(tab.id, true);
 });
--- content.js
+++ content.js
@@ -1,26 +1,14 @@
-// Select the div that contains all the content of the "accepted" answer
-const answer = document.querySelector(".accepted-answer");
-// Select all the code snippets on the page and store it as a NodeList into "codes" variable
-const codes = document.querySelectorAll("pre code");
- 
 /**
  * Function that copies a code snippet to clipboard
  * @param {String} snippet The content of the snippet
  */
-const copyCode = (snippet) => {
-  // Create a TextArea HTML element
+ const copyCode = (snippet) => {
   let input = document.createElement("textarea");
-  // Append the newly created TextArea to the page
   document.body.appendChild(input);
-  // Set the content
   input.textContent = snippet;
-  // Select the content
   input.select();
-  // Copy to clipboard
   document.execCommand("copy");
-  // Deselect the content
   input.blur();
-  // Remove the newly created tag
   input.remove();
 };
  
@@ -43,43 +31,51 @@ const displayMessage = (
     margin: "20px",
   }
 ) => {
-  // Create an HTML element which will contain our message
   let text = document.createElement(tag);
-  // Apply the style
   text.style.color = style.color;
   text.style.float = style.float;
   text.style.margin = style.margin;
-  // Set the content
   text.innerHTML = content;
-  // Add our newly created tag to a parent node
   element.appendChild(text);
-  // Delete the message after a givent time
   setTimeout(() => text.remove(), timeout);
 };
- 
-// Loop trough all the codes snippets that are on the page
-for (let i = 0; i < codes.length; i++) {
-  // Store all the content of the current snippet into "snippet" variable
-  let snippet = codes[i].innerHTML;
-  // Set the style of the cursor to "pointer"
-  codes[i].parentNode.style.cursor = "pointer";
-  // Add an event listener on all the snippets that will listen for a double click
-  codes[i].parentNode.addEventListener("dblclick", (e) => {
-    // Copy the content
+
+/**
+ * Utility function to calculate the offset of an element relative to the page
+ * @param {HTMLElement} el The HTML element
+ * @returns Object: { top, left }
+ */
+const offset = (el) => {
+  var rect = el.getBoundingClientRect(),
+  scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+  scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+};
+
+const codes = document.querySelectorAll("pre code");
+
+// Give all code snippets the "copy to clipboard" super power
+codes.forEach((code) => {
+  let snippet = code.innerHTML;
+  code.parentNode.style.cursor = "pointer";
+  code.parentNode.addEventListener("dblclick", (e) => {
     copyCode(snippet);
-    // Display that copy has worked well
     displayMessage("p", e.target, "📋 Successfully copied to clipboard");
   });
-}
+})
  
 // Listen to a message that can be sent by the background script
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  // If there's an "accepted" answer on the page
-  if (answer !== null) {
-    // Scroll smoothly to it
-    answer.scrollIntoView({ behavior: "smooth" });
+  const answer = document.querySelector(".accepted-answer");
+  const headerHeight = 47;
+  
+  if (answer) {
+    let scrollPosition = offset(answer).top - headerHeight;
+    window.scrollTo({
+      top: scrollPosition,
+      behavior: "smooth"
+    });
   } else {
-    // If not, tell the user there's none
     alert("🤖 Sadly, there's no official answer on this page");
   }
 });
